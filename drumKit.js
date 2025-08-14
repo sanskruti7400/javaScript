cab52c9aa195e6ef03dac98b61c20148d98e6ede
Sanskruti Dhanaji Kadam <sanskadam7400@gmail.com>
2025-08-14 07:43:09 -0700

    Update drumKit.js

--- drumKit.js
+++ drumKit.js
@@ -121,6 +121,7 @@ footer {
 }
 
 //index.js
+//eventListner by pressing keys
 for(var i=0;i<document.querySelectorAll(".drum").length;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
 
@@ -156,8 +157,47 @@ for(var i=0;i<document.querySelectorAll(".drum").length;i++){
                 audio.play();
                 break;
             default:
-            
+
         }
     });
 }
+//eventListner by pressing keys of keyboard
+document.addEventListener("keypress",function(event){
 
+    makeSound(event.key);
+})
+
+function makeSound(key){
+    switch(key){
+            case "w":
+                var audio=new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+            case "a":
+                var audio=new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+            case "s":
+                var audio=new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+            case "d":
+                var audio=new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "j":
+                var audio=new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "k":
+                var audio=new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case "l":
+                var audio=new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+            default:
+
+        }
+}
